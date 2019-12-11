4985d56025832f8f3d1ca16bda8ea5d2a726b538
moking1997 <moking1997@outlook.com>
2019-12-11 23:49:16 +0800

    add orm test

--- server/routers/api/index.js
+++ server/routers/api/index.js
@@ -1,4 +1,10 @@
 const Router = require('koa-router')
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('jol', 'root', '123456', {
+    host: 'localhost',
+    dialect: 'mysql' ,
+});
 
 let router = new Router()
 
@@ -14,5 +20,37 @@ router.get('/catalog/:parent', async ctx => {
     ctx.body = await ctx.db.query('SELECT ID, title FROM catalog_table WHERE parentID=?', [parent])
 })
 
-
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+const problem = sequelize.define('problem', {
+    // 属性
+    problem_id: {
+        type: Sequelize.BIGINT(11),
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING
+        // allowNull 默认为 true
+    }
+}, {
+        freezeTableName: true,
+        //去掉默认的添加时间和更新时间
+        timestamps: false,
+        indexes: [
+            //普通索引,默认BTREE
+            {
+                unique: true,
+                fields: ['pid']
+            },
+        ]
+});
+problem.findAll().then(problems => {
+    console.log("All users:", JSON.stringify(problems, null, 4));
+});
 module.exports = router.routes()
